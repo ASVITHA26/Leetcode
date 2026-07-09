9a91e869a04c97966567018ce7ad6bb08752aedc
ASVITHA26 <asvithak26@gmail.com>
2026-07-09 18:24:32 +0530

    Time: 25 ms (94.26%), Space: 100.5 MB (63.78%) - LeetHub

---
+++ 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+class RandomizedSet {
+    private List<Integer> nums;
+    private Map<Integer, Integer> indexMap;
+    private Random random;
+
+    public RandomizedSet() {
+        nums = new ArrayList<>();
+        indexMap = new HashMap<>();
+        random = new Random();
+    }
+
+    public boolean insert(int val) {
+        if (indexMap.containsKey(val)) {
+            return false;
+        }
+
+        nums.add(val);
+        indexMap.put(val, nums.size() - 1);
+        return true;
+    }
+
+    public boolean remove(int val) {
+        if (!indexMap.containsKey(val)) {
+            return false;
+        }
+
+        int index = indexMap.get(val);
+        int lastElement = nums.get(nums.size() - 1);
+
+        nums.set(index, lastElement);
+        indexMap.put(lastElement, index);
+
+        nums.remove(nums.size() - 1);
+        indexMap.remove(val);
+
+        return true;
+    }
+
+    public int getRandom() {
+        return nums.get(random.nextInt(nums.size()));
+    }
+}
