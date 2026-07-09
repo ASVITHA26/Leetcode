9267cd0f0095fed2716c7248fb64f861c7ce86e1
ASVITHA26 <asvithak26@gmail.com>
2026-07-09 18:00:34 +0530

    Time: 1 ms (99.91%), Space: 43.1 MB (85.56%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isValid(String s) {
+        int[] ans = new int[s.length()];
+        int total =-1;
+        for(char c : s.toCharArray()){
+            if(c == '('){
+                ans[++total] = ')';
+            }
+            else if(c == '{'){
+                ans[++total]= '}';
+            }
+            else if(c =='['){
+                ans[++total] =']';
+            }
+            else if( total == -1 || ans[total--] != c){
+                return false;
+            }
+        }
+        return total == -1;
+    }
+}
